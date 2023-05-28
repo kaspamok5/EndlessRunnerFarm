c131fff9f7d8079925ecce0c6090ca288f3f69ad
kaspamok5 <123108075+kaspamok5@users.noreply.github.com>
2023-05-28 18:25:42 +0300

    added collision with obstacle

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -4,6 +4,7 @@ using Unity.VisualScripting;
 using UnityEditor;
 using UnityEngine;
 using DG.Tweening;
+using UnityEngine.SceneManagement;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -11,6 +12,9 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody rb;
     public float runningSpeed = 8;
     private Vector3 vel;
+    private bool isJumping = false;
+    public float jumpForce = 2;
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
@@ -33,18 +37,36 @@ public class PlayerMovement : MonoBehaviour
             vel += Vector3.right * runningSpeed;
             StartCoroutine(StopTurning());
         }
-        /*if (!Physics.Raycast(transform.position, Vector3.down, out var hit, 1.05f))
+        if (!Physics.Raycast(transform.position, Vector3.down, out var hit, 1.2f))
         {
             Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.down) * hit.distance, Color.yellow);
-            vel += Vector3.down;
-        }*/
+            vel += Vector3.down * .2f;
+        }
+        if (Input.GetKeyDown(KeyCode.Space) && !isJumping)
+        {
+            isJumping = true;
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+        //transform.position = new Vector3( Mathf.Clamp(transform.position.x, -2.7f, 2.7f),transform.position.y,transform.position.z);
         rb.velocity = vel;
     }
 
     IEnumerator StopTurning()
     {
-        yield return new WaitForSeconds(.35f);
+        yield return new WaitForSeconds((runningSpeed / 2.7f) - 2.7f);
         print("done");
         vel = Vector3.forward * runningSpeed;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isJumping = false;
+        }
     }
 }
